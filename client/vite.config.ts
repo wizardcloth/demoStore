f602b2ed6476ac7b8fa667ac87b3d33189f70fd7
wizardcloth <ujjwal.chauhan8130@gmail.com>
2025-01-12 22:09:04 +0530

    first commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:"http://localhost:8080/"
+        target:"https://demo-store-omega.vercel.app/api"
       }
     }
   }
